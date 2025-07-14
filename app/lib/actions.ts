f2389fecfa8ff455fb549665371162df1af30537
Christian Delos Reyes <christian.dreyes@gmail.com>
2025-07-14 15:00:25 -0400

    adding revalidate

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,6 +1,10 @@
 "use server";
 
 import { z } from "zod";
+import { revalidatePath } from "next/cache";
+import postgres from "postgres";
+
+const sql = postgres(process.env.POSTGRES_PRISMA_URL!, { ssl: "require" });
 
 const FormSchema = z.object({
   id: z.string(),
@@ -19,4 +23,10 @@ export async function createInvoice(formData: FormData) {
     status: formData.get("status"),
   });
   const amountInCents = amount * 100;
+  const date = new Date().toISOString().split("T")[0];
+
+  await sql`
+    INSERT INTO invoices (customer_id, amount, status, date)
+    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+  `;
 }
