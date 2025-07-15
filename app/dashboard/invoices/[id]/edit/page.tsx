329e207a083a9b04578bfba049e7cf7fd3d58bd7
Christian Delos Reyes <christian.dreyes@gmail.com>
2025-07-15 13:51:49 -0400

    implement not found

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -10,6 +10,10 @@ export default async function Page(props: { params: Promise<{ id: string }> }) {
     fetchCustomers(),
   ]);
 
+  if (!invoice) {
+    notFound();
+  }
+
   return (
     <main>
       <Breadcrumbs
